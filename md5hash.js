03dca5383ae41c12be8abff2d52011c0f2abff6c
valtervilmerson <valter.vilmerson@gmail.com>
2022-09-03 13:24:54 -0300

    generator interface

--- md5hash.js
+++ md5hash.js
@@ -1,17 +1,16 @@
-const hash = require("md5")
 const config = require("./config.json")
 
-const limit = 50
+const element = document.getElementById("run")
+element.addEventListener("click", md5fier())
 
+const limit = 50
 const password = ""
 
 function md5fier() {
-  firstStep = hash(config.value)
+  firstStep = hash(config.value ?? "seed")
   secondStep = hash(password)
   thirdStep = firstStep + secondStep
   fourthStep = hash(thirdStep)
   newPassword = fourthStep.substring(0, limit)
   console.log(newPassword)
 }
-
-md5fier()
